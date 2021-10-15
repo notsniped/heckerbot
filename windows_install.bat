d2d4826e0c8fca7e52a2bd4633302467327ccf9f
sniperking3335 <80844436+sniperking3335@users.noreply.github.com>
2021-10-15 05:59:40 +0300

    Create windows_install.bat

---
+++ windows_install.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Installing....
+timeout 1 /nobreak > nul
+pip install praw
+pip install 
+pip install discord-py-slash-commmand
+pip install pickle
+pip install requests
+cls
+echo Complete! Press any key to exit
+pause > nul
+exit /b
