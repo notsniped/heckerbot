9884da74f64c9df313229430e46e1d49c7b45a67
sniperking3335 <80844436+sniperking3335@users.noreply.github.com>
2021-10-15 06:15:15 +0300

    Update linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -1,1 +1,37 @@
-#! /bin/sh
+#!/bin/bash
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw()
+{
+    exit $1
+}
+
+function catch()
+{
+    export exception_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $exception_code
+}
+echo "- Installing modules..."
+try
+(
+    pip install discord > /dev/null
+    echo "- Installed discord!"
+    pip install praw > /dev/null
+    echo "- Installed praw!"
+    pip install discord-py-slash-command > /dev/null
+
+
+)
+catch || {
+   case $ex_code in
+        *)
+            echo "! Error installing modules"
+            throw $ex_code
+        ;;
+    esac
+}
