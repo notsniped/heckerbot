c09c1e5ce35650e8138fdb42a65afa6c8b768f82
sniperking3335 <80844436+sniperking3335@users.noreply.github.com>
2021-10-23 13:58:04 +0000

    Update windows_install.bat

--- windows_install.bat
+++ windows_install.bat
@@ -1,11 +1,11 @@
 @echo off
 echo Installing....
 timeout /t 1 /nobreak > nul
-pip install praw
-pip install discord
-pip install discord-py-slash-commmand
-pip install pickle
-pip install requests
+pip install praw > nul
+pip install discord > nul
+pip install discord-py-slash-commmand > nul
+pip install pickle > nul
+pip install requests > nul
 cls
 echo Complete! Press any key to exit
 pause > nul
